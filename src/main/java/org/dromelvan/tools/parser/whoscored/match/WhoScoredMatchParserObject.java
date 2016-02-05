8ba589956af0a97ad4a5fa0075bafae3dca68c27
dromelvan <dromelvan@aland.net>
2016-02-05 11:55:00 +0200

    Better fix for match date/time format change in WhoScored match page.

--- src/main/java/org/dromelvan/tools/parser/whoscored/match/WhoScoredMatchParserObject.java
+++ src/main/java/org/dromelvan/tools/parser/whoscored/match/WhoScoredMatchParserObject.java
@@ -1,24 +1,34 @@
 package org.dromelvan.tools.parser.whoscored.match;
 
 import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import org.dromelvan.tools.parser.match.MatchParserObject;
 import org.joda.time.DateTime;
 import org.joda.time.format.DateTimeFormat;
 import org.joda.time.format.DateTimeFormatter;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class WhoScoredMatchParserObject extends MatchParserObject {
 
 	private int whoScoredId;
+	private final static Logger logger = LoggerFactory.getLogger(WhoScoredMatchParserObject.class);
 
 	public WhoScoredMatchParserObject(Map match) {
 		setWhoScoredId((Integer) match.get(WhoScoredMatchJavaScriptVariables.MATCH_ID));
 
 		Map matchCentreData = (Map) match.get(WhoScoredMatchJavaScriptVariables.MATCH_CENTRE_DATA);
-
-		DateTimeFormatter dateTimeFormat = DateTimeFormat.forPattern("MM/dd/YYYY HH:mm:ss 'PM'");
-		DateTime dateTime = DateTime.parse((String) matchCentreData.get(WhoScoredMatchJavaScriptVariables.START_TIME), dateTimeFormat);
-		setDateTime(dateTime.toString());
+		Pattern startTimePattern = Pattern.compile("(\\d{1,2}/\\d{1,2}/\\d{4} \\d{1,2}:\\d{1,2}:\\d{1,2}).*");
+		Matcher startTimeMatcher = startTimePattern.matcher((String) matchCentreData.get(WhoScoredMatchJavaScriptVariables.START_TIME));
+		if (startTimeMatcher.matches()) {
+			DateTimeFormatter dateTimeFormat = DateTimeFormat.forPattern("MM/dd/YYYY HH:mm:ss");
+			DateTime dateTime = DateTime.parse(startTimeMatcher.group(1), dateTimeFormat);
+			setDateTime(dateTime.toString());
+		} else {
+			logger.warn("Could not parse start time from input {}.", matchCentreData.get(WhoScoredMatchJavaScriptVariables.START_TIME));
+		}
 
 		setTimeElapsed((String) matchCentreData.get(WhoScoredMatchJavaScriptVariables.ELAPSED));
 
